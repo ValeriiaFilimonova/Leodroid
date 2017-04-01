5e16410fe047a27d51b0812b86b6ed4ddbaeaf80
ValeriiaFilimonova <valeriya.filimonova@akvelon.com>
2017-04-01 16:37:23 +0300

    Fixed compile script for managing service and united all services in target

--- systemd.sh
+++ systemd.sh
@@ -13,15 +13,15 @@ SPEECH_RECOGNITION_NAME="SpeechRecognitionService"
 SPEECH_RECOGNITION_UNIT_FILE="
 [Unit]
 Description=Java Sphinx Speech Recognition Service
+Requires=rabbitmq-server.service
+StopWhenUnneeded=true
 
 [Service]
 Type=simple
 Restart=always
 Environment=\"MODELS_PATH=${SERVICES_PATH}/models/\"
 ExecStart=/usr/bin/java -cp ${LIB_PATH}/*:${LIB_PATH}/${SPEECH_RECOGNITION_NAME}/*:${SERVICES_PATH}/${SPEECH_RECOGNITION_NAME}.jar RecognitionService
-
-[Install]
-WantedBy=multi-user.target"
+"
 
 echo -e "$SPEECH_RECOGNITION_UNIT_FILE" > $SYSTEMD_PATH/$SPEECH_RECOGNITION_SERVICE_NAME
 chmod 664 $SYSTEMD_PATH/$SPEECH_RECOGNITION_SERVICE_NAME
@@ -33,14 +33,14 @@ SPEECH_SYNTHESIS_NAME="SpeechSynthesisService"
 SPEECH_SYNTHESIS_UNIT_FILE="
 [Unit]
 Description=C++ Espeak Based Speech Synthesis Service
+Requires=rabbitmq-server.service
+StopWhenUnneeded=true
 
 [Service]
 Type=simple
 Restart=always
 ExecStart=${SERVICES_PATH}/${SPEECH_SYNTHESIS_NAME}
-
-[Install]
-WantedBy=multi-user.target"
+"
 
 echo -e "$SPEECH_SYNTHESIS_UNIT_FILE" > $SYSTEMD_PATH/$SPEECH_SYNTHESIS_SERVICE_NAME
 chmod 664 $SYSTEMD_PATH/$SPEECH_SYNTHESIS_SERVICE_NAME
@@ -52,15 +52,14 @@ DATA_STORAGE_NAME="DataStorageService"
 DATA_STORAGE_UNIT_FILE="
 [Unit]
 Description=NodeJS Redis Based Data Storage Service
-Requires=redis.service
+Requires=redis-server.service
+StopWhenUnneeded=true
 
 [Service]
 Type=simple
 Restart=always
 ExecStart=${SERVICES_PATH}/${DATA_STORAGE_NAME}
-
-[Install]
-WantedBy=multi-user.target"
+"
 
 echo -e "$DATA_STORAGE_UNIT_FILE" > $SYSTEMD_PATH/$DATA_STORAGE_SERVICE_NAME
 chmod 664 $SYSTEMD_PATH/$DATA_STORAGE_SERVICE_NAME
@@ -73,24 +72,30 @@ APPLICATION_MANAGING_UNIT_FILE="
 [Unit]
 Description=Java Systemctl Based Managing Service
 Requires=${SPEECH_RECOGNITION_SERVICE_NAME} ${SPEECH_SYNTHESIS_SERVICE_NAME} ${DATA_STORAGE_SERVICE_NAME}
+StopWhenUnneeded=true
 
 [Service]
 Type=simple
 Restart=always
-ExecStart=/usr/bin/java -cp ${LIB_PATH}/*:${SERVICES_PATH}/${APPLICATION_MANAGING_NAME}.jar ManagingService
-
-[Install]
-WantedBy=multi-user.target"
+ExecStart=/usr/bin/java -cp ${LIB_PATH}/*:${LIB_PATH}/${APPLICATION_MANAGING_NAME}/*:${SERVICES_PATH}/${APPLICATION_MANAGING_NAME}.jar ManagingService
+"
 
 echo -e "$APPLICATION_MANAGING_UNIT_FILE" > $SYSTEMD_PATH/$APPLICATION_MANAGING_SERVICE_NAME
-chmod 664 $SYSTEMD_PATH/$APPLICATION_MANAGING_SERVICE_NAME
+chmod 664 $SYSTEMD_PATH/$APPLICATION_MANAGING_SERVICE_NAMEu
+
+#Target to unite all services
+
+TARGET_UNIT_FILE="
+[Unit]
+Description=Droid System Built-In Services Target
+Requires=$SPEECH_RECOGNITION_SERVICE_NAME $SPEECH_SYNTHESIS_SERVICE_NAME $APPLICATION_MANAGING_SERVICE_NAME $DATA_STORAGE_SERVICE_NAME
+After=$SPEECH_RECOGNITION_SERVICE_NAME $SPEECH_SYNTHESIS_SERVICE_NAME $APPLICATION_MANAGING_SERVICE_NAME $DATA_STORAGE_SERVICE_NAME
+"
+echo -e "$TARGET_UNIT_FILE" > $SYSTEMD_PATH/droid-builtin.target
+chmod 664 $SYSTEMD_PATH/droid-builtin.target
 
 echo "unit files created"
 
 systemctl daemon-reload
-systemctl reenable $SPEECH_RECOGNITION_SERVICE_NAME
-systemctl reenable $SPEECH_SYNTHESIS_SERVICE_NAME
-systemctl reenable $APPLICATION_MANAGING_SERVICE_NAME
-systemctl reenable $DATA_STORAGE_SERVICE_NAME
 
 echo "services enabled"
