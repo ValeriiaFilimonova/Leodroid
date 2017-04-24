11c3da3788ae3684c3bba87fbb3e3d8962583b0c
ValeriiaFilimonova <valeriya.filimonova@akvelon.com>
2017-04-24 10:01:52 +0300

    Added systemd configuration for maintenance service

--- DataStorageService/src/DefaultServices.js
+++ DataStorageService/src/DefaultServices.js
@@ -3,8 +3,6 @@ module.exports = [
         applicationName: 'Droid Application Managing Service',
         serviceName: 'application-managing',
         description: 'Java Systemctl Based Managing Service',
-        mainClass: 'ManagingService',
-        dependencies: true,
         commands: {
             rules: {
                 startApp: {
@@ -21,22 +19,15 @@ module.exports = [
             }
         }
     },
-    // {
-    //     applicationName: 'Droid Data Storage Service',
-    //     serviceName: 'data-storage',
-    //     description: 'NodeJS Redis Based Data Storage Service'
-    // },
-    // {
-    //     applicationName: 'Droid Maintenance Service',
-    //     serviceName: 'application-maintenance',
-    //     description: 'NodeJS + Bash Maintenance Service'
-    // },
+    {
+        applicationName: 'Droid Maintenance Service',
+        serviceName: 'application-maintenance',
+        description: 'NodeJS Service for Installing/Uninstalling of New Apps'
+    },
     {
         applicationName: 'Droid Speech Recognition Service',
         serviceName: 'speech-recognition',
         description: 'Java Sphinx Speech Recognition Service',
-        mainClass: 'RecognitionService',
-        dependencies: true
     },
     {
         applicationName: 'Droid Speech Synthesis Service',
--- compile.sh
+++ compile.sh
@@ -4,10 +4,12 @@ cd $(dirname $0)
 source env.opts
 
 COMPILE_FILE="compile.sh"
-DIRECTORIES=($SPEECH_RECOGNITION_NAME $SPEECH_SYNTHESIS_NAME $DATA_STORAGE_NAME $APPLICATION_MANAGING_NAME)
+DIRECTORIES=($SPEECH_RECOGNITION_NAME $SPEECH_SYNTHESIS_NAME $DATA_STORAGE_NAME $APPLICATION_MANAGING_NAME $MAINTENANCE_NAME)
 
 for directory in "${DIRECTORIES[@]}"
 do
-    mkdir "exe/$directory"
+    if [[ ! -d "exe/$directory" ]]; then
+        mkdir "exe/$directory"
+    fi;
     /bin/bash $directory/$COMPILE_FILE
 done
