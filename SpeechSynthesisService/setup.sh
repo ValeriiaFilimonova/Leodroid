2a4c795dd4a497117ac836172db0ba83a4c118fb
ValeriiaFilimonova <valeriya.filimonova@akvelon.com>
2017-04-29 15:26:29 +0300

    Fixed SSS setup script

--- SpeechSynthesisService/setup.sh
+++ SpeechSynthesisService/setup.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
-# AMQP-CPP install
+# POCO install
+apt-get install -y libpoco-dev
 
+# AMQP-CPP install
 git clone https://github.com/CopernicaMarketingSoftware/AMQP-CPP
 cd AMQP-CPP
 make pure
@@ -9,7 +11,20 @@ make install
 cd ../
 rm -r AMQP-CPP
 
-# POCO install
-apt-get install cmake libpoco-dev
-
 # In file /etc/java-8-openjdk/sound.properties comment PulseAudio settings and uncomment DirectAudioProvider if LineUnavailable error
+
+#Uncomment for Yocto
+FILE_PATH="/usr/lib/jvm/java-8-openjdk/jre/lib/sound.properties"
+
+#Uncomment for Ubuntu
+#FILE_PATH="/etc/java-8-openjdk/sound.properties"
+#head -n -9 /etc/java-8-openjdk/sound.properties > /etc/java-8-openjdk/sound.properties
+#echo #javax.sound.sampled.Clip=org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider >> $FILE_PATH
+#echo #javax.sound.sampled.Port=org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider >> $FILE_PATH
+#echo #javax.sound.sampled.SourceDataLine=org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider >> $FILE_PATH
+#echo #javax.sound.sampled.TargetDataLine=org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider >> $FILE_PATH
+
+echo javax.sound.sampled.Clip=com.sun.media.sound.DirectAudioDeviceProvider >> $FILE_PATH
+echo javax.sound.sampled.Port=com.sun.media.sound.PortMixerProvider >> $FILE_PATH
+echo javax.sound.sampled.SourceDataLine=com.sun.media.sound.DirectAudioDeviceProvider >> $FILE_PATH
+echo javax.sound.sampled.TargetDataLine=com.sun.media.sound.DirectAudioDeviceProvider >> $FILE_PATH
